d708b0ed7a385034a2c21f6bbed6b5d901aba296
jzh <jzh879869954@qq.com>
2026-07-24 17:45:54 +0800

    优化终端配色方案

--- tabby-terminal/src/colorSchemes.ts
+++ tabby-terminal/src/colorSchemes.ts
@@ -57,13 +57,13 @@ export class DefaultColorSchemes extends TerminalColorSchemeProvider {
     static jzhNightColorScheme: TerminalColorScheme = {
         name: 'jzh Night',
         foreground: '#a9b1d6',
-        background: '#23242f',
+        background: '#1a1b26',
         cursor: '#a9b1d6',
         selection: '#33467c',
-        selectionForeground: '#c0caf5',
-        cursorAccent: '#23242f',
+        selectionForeground: '#a9b1d6',
+        cursorAccent: '#1a1b26',
         colors: [
-            '#1a1b26',
+            '#23242f',
             '#f7768e',
             '#9ece6a',
             '#e0af68',
@@ -71,14 +71,42 @@ export class DefaultColorSchemes extends TerminalColorSchemeProvider {
             '#bb9af7',
             '#7dcfff',
             '#a9b1d6',
-            '#414868',
+            '#23242f',
             '#f7768e',
             '#9ece6a',
             '#e0af68',
             '#7aa2f7',
             '#bb9af7',
             '#7dcfff',
-            '#c0caf5',
+            '#a9b1d6',
+        ],
+    }
+
+    static jzhLatteColorScheme: TerminalColorScheme = {
+        name: 'jzh Latte',
+        foreground: '#acb0be',
+        background: '#eff1f5',
+        cursor: '#acb0be',
+        selection: '#ccd0da',
+        selectionForeground: '#5c5f77',
+        cursorAccent: '#eff1f5',
+        colors: [
+            '#5c5f77',
+            '#d20f39',
+            '#40a02b',
+            '#df8e1d',
+            '#1e66f5',
+            '#ea76cb',
+            '#179299',
+            '#acb0be',
+            '#5c5f77',
+            '#d20f39',
+            '#40a02b',
+            '#df8e1d',
+            '#1e66f5',
+            '#ea76cb',
+            '#179299',
+            '#acb0be',
         ],
     }
 
@@ -87,6 +115,7 @@ export class DefaultColorSchemes extends TerminalColorSchemeProvider {
             DefaultColorSchemes.defaultColorScheme,
             DefaultColorSchemes.defaultLightColorScheme,
             DefaultColorSchemes.jzhNightColorScheme,
+            DefaultColorSchemes.jzhLatteColorScheme,
         ]
     }
 }
